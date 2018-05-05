0706b07138a94eaaa112c2961b7a5b5ba1845b0a
morozov-a <mauk.lekph@gmail.com>
2018-05-05 19:50:00 +0300

    update DB(Age,Culture,FirstName,LastName,Theme)

--- Course_Project/Data/Migrations/20180505164530_MigrateSomeForUsersDB.Designer.cs
+++ Course_Project/Data/Migrations/20180505164530_MigrateSomeForUsersDB.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Course_Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180505164530_MigrateSomeForUsersDB")]
+    partial class MigrateSomeForUsersDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Course_Project/Models/ApplicationUser.cs
+++ Course_Project/Models/ApplicationUser.cs
@@ -9,5 +9,10 @@ namespace Course_Project.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Theme { get; set; }
+        public string Culture { get; set; }
     }
 }
